649680b8948dfd5807226954bc7a84b9f09342cb
Narendra B T <narendrabt93@gmail.com>
2025-08-13 21:13:02 -0400

    added view engine to handlebars

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.set('view engine', 'handlebars');
+
 // MongoDB connection (execute once globally)
 let isConnected = false;
 async function connectDB() {
